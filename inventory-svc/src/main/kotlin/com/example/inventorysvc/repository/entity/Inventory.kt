cb5b863ed70fc344f6c509d3a12495c68eda6467
lifeodyssey <zhouthepassion@outlook.com>
2024-01-25 16:57:23 +0800

    feat: add inventory svc db config

---
+++ inventory-svc/src/main/kotlin/com/example/inventorysvc/repository/entity/Inventory.kt
@@ -0,0 +1,11 @@
+package com.example.inventorysvc.repository.entity
+
+// @Entity
+// @Table
+// data class Inventory(
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    val id: Long? = null,
+//    val skuCode: String,
+//    var quantity: Int
+// )
