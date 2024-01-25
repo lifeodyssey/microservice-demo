f6e22c2545a9712e13eeb511bbcde787641e44d6
lifeodyssey <zhouthepassion@outlook.com>
2024-01-25 16:56:59 +0800

    feat: init inventory svc

---
+++ inventory-svc/src/main/kotlin/com/example/inventorysvc/InventorySvcApplication.kt
@@ -0,0 +1,11 @@
+package com.example.inventorysvc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InventorySvcApplication
+
+fun main(args: Array<String>) {
+    runApplication<InventorySvcApplication>(*args)
+}
